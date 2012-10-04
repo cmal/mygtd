3559c45364b24d5dcb5bcbdc503293b8c5534ee9
cmal <zyzy5730@163.com>
2012-10-04 12:38:45 +0800

    新文件：    ChangeLog.txt 	修改：      README.md 	新文件：    mygtd.py

---
+++ mygtd.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#==============================================================================
+#
+# This file is a part of mygtd
+#
+# File: mygtd.py
+# Description: Main app.
+# Author: Yu Zhao 赵宇 <zyzy5730@163.com>
+# Created: 2012-10-04 12:21:26
+# Last modified: 2012-10-04 12:29:53
+#
+# Copyright (C) 2012-2013 Yu Zhao.
+#
+#==============================================================================
+
+
+class MyGtdApp(wx.App):
+    '''App class for MyGtd.'''
+    def OnInit(self):
+        self.frame = MyGtdWindow(None, -1, 'MyGtd',
+                wx.DEFAULT_FRAME_STYLE|wx.NO_FULL_REPAINT_ON_RESIZE)
+        self.frame.Show(True)
+        self.SetTopWindow(self.frame)
+        return True
+
+if __name__ == "__main__":
+    # if called as a script, and not as a module
+    app = MyGtd()
+    app.MainLoop()
