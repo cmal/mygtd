43ea2f67718a206b16a147c70eb7e31b9d2fcb17
cmal <zyzy5730@163.com>
2012-10-04 13:00:28 +0800

    新文件：    Lib/window.py

---
+++ Lib/window.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#==============================================================================
+#
+# This file is a part of mygtd
+#
+# File: window.py
+# Description: mygtd Window.
+# Author: Yu Zhao 赵宇 <zyzy5730@163.com>
+# Created: 2012-10-04 12:46:20
+# Last modified: 2012-10-04 12:46:42
+#
+# Copyright (C) 2012-2013 Yu Zhao.
+#
+#==============================================================================
+
